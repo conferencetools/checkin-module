fde1740874b79f32aaa3bebaa1858fafb3359be9
Carnage <t.carnage@gmail.com>
2018-02-07 08:42:21 +0000

    Added JMS annotations to commands

--- src/Domain/Command/Delegate/RegisterDelegate.php
+++ src/Domain/Command/Delegate/RegisterDelegate.php
@@ -5,18 +5,24 @@ namespace ConferenceTools\Checkin\Domain\Command\Delegate;
 use Carnage\Cqrs\Command\CommandInterface;
 use ConferenceTools\Checkin\Domain\ValueObject\DelegateInfo;
 use ConferenceTools\Checkin\Domain\ValueObject\Ticket;
+use JMS\Serializer\Annotation as JMS;
 
 class RegisterDelegate implements CommandInterface
 {
     /**
      * @var DelegateInfo
+     * @JMS\Type("ConferenceTools\Checkin\Domain\ValueObject\DelegateInfo")
      */
     private $delegateInfo;
     /**
      * @var Ticket
+     * @JMS\Type("ConferenceTools\Checkin\Domain\ValueObject\Ticket")
      */
     private $ticket;
-
+    /**
+     * @var string
+     * @JMS\Type("string")
+     */
     private $purchaserEmail;
 
     public function __construct(
--- src/Domain/Command/Delegate/UpdateDelegateInformation.php
+++ src/Domain/Command/Delegate/UpdateDelegateInformation.php
@@ -4,15 +4,18 @@ namespace ConferenceTools\Checkin\Domain\Command\Delegate;
 
 use Carnage\Cqrs\Command\CommandInterface;
 use ConferenceTools\Checkin\Domain\ValueObject\DelegateInfo;
+use JMS\Serializer\Annotation as JMS;
 
 class UpdateDelegateInformation implements CommandInterface
 {
     /**
      * @var DelegateInfo
+     * @JMS\Type("ConferenceTools\Checkin\Domain\ValueObject\DelegateInfo")
      */
     private $delegateInfo;
     /**
      * @var string
+     * @JMS\Type("string")
      */
     private $delegateId;
 
