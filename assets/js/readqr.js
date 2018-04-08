f2ad4376fe16a9c7d8c3b9523de282e7cbbce8cd
Carnage <t.carnage@gmail.com>
2018-04-08 16:26:15 +0100

    Added more controls to QR code scanner

--- assets/js/readqr.js
+++ assets/js/readqr.js
@@ -1,21 +1,60 @@
 $(document).on('ready', function () {
 
     var scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+    var cameras = [];
+    var camIdx = 0;
     scanner.addListener('scan', function (content) {
         $('input[name="ticketId"]').val(content);
         $('#qr').hide();
         $('button[name="submit"]').click();
         scanner.stop();
     });
-    Instascan.Camera.getCameras().then(function (cameras) {
+
+    $('#qr-stop').click(
+        function () {
+            scanner.stop();
+            $(this).hide();
+            $('#qr-start').show();
+        }
+    );
+
+    $('#qr-start').click(
+        function () {
+            scan(cameras[camIdx]);
+            $(this).hide();
+            $('#qr-stop').show();
+        }
+    );
+
+    $('#qr-switch-camera').click(
+        function () {
+            camIdx = (camIdx + 1) % cameras.length;
+            scan(cameras[camIdx]);
+        }
+    );
+
+    function scan(camera) {
+        scanner.stop();
+        scanner.start(camera)
+            .then(function (e) {$('#qr').show();})
+            .catch(function(e) {$('#qr').hide();});
+    }
+
+    Instascan.Camera.getCameras().then(function (cams) {
+        cameras = cams;
+        if (cameras.length > 1) {
+            $('#qr-switch-camera').click(function() {});
+        } else {
+            $('#qr-switch-camera').hide();
+        }
         if (cameras.length > 0) {
-            scanner.start(cameras[0]);
-            $('#qr').show();
+            scanner.start(cameras[0])
+                .then(function (e) {$('#qr').show();})
+                .catch(function(e) {$('#qr').hide();});
         } else {
             console.error('No cameras found.');
         }
     }).catch(function (e) {
         console.error(e);
     });
-
 });
