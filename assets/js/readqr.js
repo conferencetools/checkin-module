703be60535aba8d23fb84be5bb9af3e8c163f0c8
Carnage <t.carnage@gmail.com>
2018-04-08 16:42:53 +0100

    Added few tweaks to styling, set some better defaults on the scanner

--- assets/js/readqr.js
+++ assets/js/readqr.js
@@ -1,6 +1,6 @@
 $(document).on('ready', function () {
 
-    var scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+    var scanner = new Instascan.Scanner({ video: document.getElementById('preview') }, {mirror: false, backgroundScan: false, scanPeriod:5});
     var cameras = [];
     var camIdx = 0;
     scanner.addListener('scan', function (content) {
@@ -21,36 +21,35 @@ $(document).on('ready', function () {
     $('#qr-start').click(
         function () {
             scan(cameras[camIdx]);
-            $(this).hide();
-            $('#qr-stop').show();
-        }
-    );
-
-    $('#qr-switch-camera').click(
-        function () {
-            camIdx = (camIdx + 1) % cameras.length;
-            scan(cameras[camIdx]);
         }
     );
 
     function scan(camera) {
         scanner.stop();
         scanner.start(camera)
-            .then(function (e) {$('#qr').show();})
+            .then(function (e) {
+                $('#qr').show();
+                $('#qr-start').hide();
+                $('#qr-stop').show();
+            })
             .catch(function(e) {$('#qr').hide();});
     }
 
     Instascan.Camera.getCameras().then(function (cams) {
         cameras = cams;
         if (cameras.length > 1) {
-            $('#qr-switch-camera').click(function() {});
+            $('#qr-switch-camera').click(
+                function () {
+                    camIdx = (camIdx + 1) % cameras.length;
+                    scan(cameras[camIdx]);
+                }
+            );
         } else {
             $('#qr-switch-camera').hide();
         }
+
         if (cameras.length > 0) {
-            scanner.start(cameras[0])
-                .then(function (e) {$('#qr').show();})
-                .catch(function(e) {$('#qr').hide();});
+            scan(cameras[camIdx]);
         } else {
             console.error('No cameras found.');
         }
